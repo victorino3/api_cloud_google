b46ef0f1c98b7682dd96b00de11c694d5d98d09f
victorino3 <victorinogomes1@gmail.com>
2023-05-30 23:50:45 +0100

    Update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { route } from "./route.js";
+import cors from "cors";
 const port = 3000;
 const app = express()
+app.use(cors());
 app.use('/',route)
 app.listen(port,() => console.log("listen ",port))
